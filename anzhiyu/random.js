460713c9b51a80453aef9cbffbd409b2f68c55e5
Answer <3513208481@qq.com>
2025-02-20 04:22:42 +0800

    Site updated: 2025-02-20 04:22:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/19/第一篇博客/"];function toRandomPost(){
+var posts=["2025/02/19/第一篇博客/","2025/02/20/所感所想/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
