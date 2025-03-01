e6557550727a946578e27a45e4e6b2710c8ad05c
Answer <3513208481@qq.com>
2025-03-01 21:10:35 +0800

    Site updated: 2025-03-01 21:10:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/19/第一篇博客/","2025/02/20/所感所想/","2025/03/01/java基础以及面向对象复习/"];function toRandomPost(){
+var posts=["2025/02/19/第一篇博客/","2025/02/20/所感所想/","2025/03/01/java基础以及面向对象复习/","2025/03/01/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
