c77f1440b550a9a1e44310171253b3a89a5698b5
Answer <3513208481@qq.com>
2025-03-03 12:32:12 +0800

    Site updated: 2025-03-03 12:32:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/19/第一篇博客/","2025/02/20/所感所想/","2025/02/20/java基础+面向对象/"];function toRandomPost(){
+var posts=["2025/02/19/第一篇博客/","2025/02/20/所感所想/","2025/02/20/java基础+面向对象/","2025/03/03/函数式编程，枚举，字符串/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
